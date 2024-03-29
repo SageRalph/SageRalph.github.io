5d6c34245701adfb5d54f33353b89329ae91befa
Sage Ralph <SageJeanRalph@gmail.com>
2024-03-29 00:11:50 +0000

    Revamp

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
  * Sets a single tab to be visible.
  */
 function tab(name) {
-    var shown = document.getElementsByClassName('visibleTab');
-    for (var i = 0; i < shown.length; i++) {
+    const shown = document.getElementsByClassName('visibleTab');
+    for (let i = 0; i < shown.length; i++) {
         shown[i].className = 'hiddenTab';
     }
     document.getElementById('tab' + name).className = 'visibleTab';
@@ -11,10 +11,10 @@ function tab(name) {
 }
 
 function selectOne(tableID, itemID) {
-    var current = document.querySelectorAll("#" + tableID + " .selected");
+    const current = document.querySelectorAll("#" + tableID + " .selected");
     if (current.length) current[0].className = '';
     if (itemID) {
-        var target = document.getElementById(itemID);
+        const target = document.getElementById(itemID);
         if (target) target.className = 'selected';
     }
 }
